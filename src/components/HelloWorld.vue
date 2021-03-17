c7e68ab31ab14de35da1c95a973e520bc5b843db
mohaned <mohaned.omran1@gmail.com>
2021-03-17 13:40:32 +0400

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,70 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   <div>
+    <b-form @submit="onSubmit" @reset="onReset">
+      
+      <b-form-group
+        id="input-group-1"
+        label="IP address :"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.ip"
+          type="text"
+          placeholder="ip"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  
+  </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from 'axios';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+ export default {
+    data() {
+      return {
+        responseV:null,
+        form: {
+          ip: '',
+        }
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+               axios.defaults.headers.common['Content-Type'] = null;
+axios.defaults.headers.post['Accept'] = 'application/json'  // default header for all POST request
+
+console.log(JSON.stringify(this.form));
+axios.post("https://api.hitpixel.io/test/whitelist", JSON.stringify(this.form), {
+            headers: {
+            }
+            })
+            .then(function(response) {
+alert(JSON.stringify(response.data));
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        },
+    
+        //alert(JSON.stringify(this.form))
+     
+      onReset(event) {
+        event.preventDefault()
+        // Reset our form values
+        this.form.ip = ''
+       
+       
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
+import VeeValidate from 'vee-validate';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import {
+  ValidationObserver,
+  ValidationProvider,
+  } from "vee-validate";
+
+Vue.use(VeeValidate);
+Vue.component("ValidationObserver", ValidationObserver);
+Vue.component("ValidationProvider", ValidationProvider);
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
